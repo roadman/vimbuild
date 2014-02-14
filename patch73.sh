607d30f485539d6391e6f76df314f906a01e1de7
seiji ochiai <roadmang@gmail.com>
2014-02-13 23:06:11 -0500

    update patch run command, all patch enable.

--- patch73.sh
+++ patch73.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-cd vim73
-cat ../patches73/* | patch -p0
+
+cd patches73
+
+patchnolist=`ls 7.3.* | cut -d"." -f3 | sort -n`
+
+cd ../vim73
+
+for patchno in $patchnolist; do
+    patchfile="../patches73/7.3.$patchno";
+#    echo $patchfile
+    cat $patchfile
+done | patch -p0
+#cat ../patches73/* | patch -p0
 
